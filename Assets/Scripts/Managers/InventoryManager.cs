096977963cab193d2b5c34b1a8442ec9905ef565
Ramninder <ramninder@ue-germany.de>
2026-06-11 15:34:28 +0200

    Setup Inventory Manager script to track tracked grocery
    - It stores the position where the selected item will be spawned in the 'counterSlots' (Transform) array.
    - It stores the detail of each item in the inventory (Item Name, Item Price, Item Image).
    - It also keeps count of the total items in the inventory.

---
+++ Assets/Scripts/Managers/InventoryManager.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+[System.Serializable]
+public class InventoryItems
+{
+    public string itemName;
+    public float itemPrice;
+    public Sprite itemImage;
+
+    public InventoryItems(string name, float price, Sprite image)
+    {
+        itemName = name;
+        itemPrice = price;
+        itemImage = image;
+    }
+}
+
+public class InventoryManager : MonoBehaviour
+{
+    public static InventoryManager Instance {  get; private set; }
+
+    public Transform[] counterSlots;
+    [HideInInspector] public List<InventoryItems> inventoryItems = new List<InventoryItems>();
+    [HideInInspector] public int totalItems = 0;
+
+    public void AddItem(string name, float price, Sprite image)
+    {
+        inventoryItems.Add(new InventoryItems(name, price, image));
+        totalItems++;
+    }
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+    }
+}
